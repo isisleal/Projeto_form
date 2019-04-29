007450d58f097aee00076fd1e40826a5e491e202
Isis <47140770+isisleal@users.noreply.github.com>
2019-04-29 12:55:01 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+ 
+
+   firebase.initializeApp({
+     apiKey: "AIzaSyDbDhbUV8XxSNrVSKDB9y7Vj6IjFx9sOZs",
+     authDomain: "projeto-form-f37bf.firebaseapp.com",
+     projectId: "projeto-form-f37bf",
+   });
+  
+   var db = firebase.firestore()
+   
+   function executar(){  
+
+  
+
+    function getRadioValue()
+    {
+        for (var i = 0; i < document.getElementsByName("sexo").length; i++)
+        {
+            if (document.getElementsByName("sexo")[i].checked)
+            {
+                return document.getElementsByName("sexo")[i].value;
+            } 
+        }
+    }
+
+    
+
+    function obterMarcados() {
+        var listaMarcados = document.getElementsByClassName("checkbox");
+        for (i = 0; i < listaMarcados.length; i++) {
+           if (listaMarcados[i].checked == true){
+                        var item = listaMarcados[i].checked
+                        return new Array(Array(indexedDB), Array[item])
+           }
+        }
+      } 
+    
+
+  
+    
+
+ db.collection("Projeto").add({
+    nome: document.getElementById("form_nome").value,
+    data: document.getElementById("form_data").value,
+    sexo: getRadioValue(),
+    endereco: document.getElementById("form_end").value,
+    complemento: document.getElementById("form_end_com").value,
+    bairro: document.getElementById("form_end_bai").value,
+    cidade: document.getElementById("form_end_cid").value,
+    estado: document.getElementById("select").value,
+    telefone: document.getElementById("form_tel").value,
+    celular: document.getElementById("form_cel").value,
+    email: document.getElementById("form_end_email").value,
+   interesses: obterMarcados(),
+    mensagem: document.getElementById("texto").value,
+    
+    
+ })
+ .then(function() {
+     console.log("Document successfully written!");
+ })
+ .catch(function(error) {
+     console.error("Error writing document: ", error);
+ });
+
+ }
+
+ 
